953dd6f0a4f4746f985645fc6f5fa2aa5d442cdd
Gustavs Iļjučonoks <gustavs.iljuconoks@cube.lv>
2024-02-08 09:12:33 +0200

    Initial commit

---
+++ resources/js/layouts/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <main class="mx-auto max-w-7xl items-center justify-between p-6 lg:px-8">
+        <router-view></router-view>
+    </main>
+</template>
+
+<script setup lang="ts">
+    import axios from "axios";
+    import {useRouter} from "vue-router";
+</script>
---
+++ resources/js/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="underline text-3xl text-red-500">This is home view</h1>
+    </div>
+</template>
