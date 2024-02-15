314cd82413fb58e17f5620a21b84bdbc5c7c7c32
hangxdd <mrlivescreamlv@gmail.com>
2024-02-15 12:12:52 +0200

    Updated some navbar styling

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,11 +1,11 @@
 <template>
-    <fwb-navbar class="w-full fixed">
+    <fwb-navbar class="w-full fixed shadow-md">
         <template #logo>
             <div class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m21 7.5-9-5.25L3 7.5m18 0-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
                 </svg>
-                <div class="p-4">
+                <div class="p-4 hidden sm:block">
                     <h1>Storage control system</h1>
                 </div>
             </div>
