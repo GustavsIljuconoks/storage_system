c90f89d2ade33bf8a11fc476f035224f046bfa2d
hangxdd <mrlivescreamlv@gmail.com>
2024-02-15 10:52:10 +0200

    Login, Register Navbar creation
    Created Login, Register pages and navigation bar for all pages.

--- resources/js/views/HomeView.vue
+++ resources/js/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <h1 class="underline text-3xl text-red-500">This is home view</h1>
+        <h1 class="flex h-screen items-center justify-center text-red-500">This is home view</h1>
     </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,55 @@ export default {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}",
+        "node_modules/flowbite/**/*.{js,jsx,ts,tsx}"
     ],
     theme: {
         extend: {
             colors: {
                 primary: {
-                    'grayIsh': '#e0e0e0'
+                    "50":"#FFFFFF","100":"#FFFFFF","200":"#FFFFFF","300":"#FFFFFF","400":"#FEF4E3","500":"#FCE4BB","600":"#FBD594","700":"#F9C56D","800":"#F7B546","900":"#F6A61F","950":"#F59E0B"
                 }
-            }
+            },
+            fontFamily: {
+                'body': [
+              'Inter', 
+              'ui-sans-serif', 
+              'system-ui', 
+              '-apple-system', 
+              'system-ui', 
+              'Segoe UI', 
+              'Roboto', 
+              'Helvetica Neue', 
+              'Arial', 
+              'Noto Sans', 
+              'sans-serif', 
+              'Apple Color Emoji', 
+              'Segoe UI Emoji', 
+              'Segoe UI Symbol', 
+              'Noto Color Emoji'
+            ],
+                'sans': [
+              'Inter', 
+              'ui-sans-serif', 
+              'system-ui', 
+              '-apple-system', 
+              'system-ui', 
+              'Segoe UI', 
+              'Roboto', 
+              'Helvetica Neue', 
+              'Arial', 
+              'Noto Sans', 
+              'sans-serif', 
+              'Apple Color Emoji', 
+              'Segoe UI Emoji', 
+              'Segoe UI Symbol', 
+              'Noto Color Emoji'
+            ]
+              }
         },
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
