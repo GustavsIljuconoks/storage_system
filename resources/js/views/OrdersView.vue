3204c3647589f8f27a25a55473547b69e8314e50
hangxdd <mrlivescreamlv@gmail.com>
2024-03-07 13:48:51 +0200

    Added search funcionality for Orders page

--- resources/js/views/OrdersView.vue
+++ resources/js/views/OrdersView.vue
@@ -55,15 +55,7 @@
                         </router-link>
                     </div>
                 </div>
-                <!-- <div class="mt-2 relative">
-                    <input type="text" id="search" class="w-full shadow-md px-3 py-2 border rounded-lg pl-10" placeholder="Search order">
-                    <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                        </svg>
-                    </div>
-                </div> -->
-                <Search placeholderText="Search Order" :isSmall="true"/>
+                <Search placeholderText="Search Order" :isSmall="true" @update:search="searchText = $event"/>
             </div>
             <div class="mt-6 overflow-auto relative">
                 <table class="min-w-full border-separate border-spacing-y-2">
@@ -78,7 +70,7 @@
                         </tr>
                     </thead>
                     <tbody class="bg-blue-200">
-                        <tr v-for="order in orders" :key="order.id">
+                        <tr v-for="order in filteredOrders" :key="order.id">
                             <td class="font-bold px-6 py-4 whitespace-nowrap">{{ order.id }}</td>
                             <td class="px-6 py-4 whitespace-nowrap">{{ order.name }}</td>
                             <td class="px-6 py-4 whitespace-nowrap">{{ order.category }}</td>
@@ -103,9 +95,10 @@
 <script setup type="ts">
     import PageLayout from '@/components/PageLayout.vue';
     import axios from "axios";
-    import { onMounted, ref } from "vue";
+    import { onMounted, ref, computed } from "vue";
     import Search from '@/components/Search.vue';
 
+    const searchText = ref('');
     const orders = ref([]);
     const activeLink = ref('');
 
@@ -186,6 +179,17 @@
                 }, 2000);
             })
     }
+
+    const filteredOrders = computed(() => {
+        if (!searchText.value) return orders.value;
+        const searchNumber = parseInt(searchText.value, 10);
+        return orders.value.filter(order => {
+            if (!isNaN(searchNumber)) {
+                return order.id === searchNumber || order.quantity === searchNumber;
+            }
+            return order.name.toLowerCase().includes(searchText.value.toLowerCase());
+        });
+    });
 
     onMounted(() => {
         getOrder();
