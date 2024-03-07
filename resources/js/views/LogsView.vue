f98acef6ef8b6649642cedd188989ba0660fe2a5
hangxdd <mrlivescreamlv@gmail.com>
2024-03-07 09:03:53 +0200

    Removed blue focus outline on logs div click

--- resources/js/views/LogsView.vue
+++ resources/js/views/LogsView.vue
@@ -29,7 +29,6 @@
                         :key="idx"
                         :class="[
                             'rounded-xl bg-white p-3',
-                            'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
                         ]"
                         >
                         <div class="w-full px-4">
@@ -97,69 +96,9 @@
     import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
 
     const categories = ref({
-        All: [
-            {
-            id: 1,
-            title: 'Does drinking coffee make you smarter?',
-            date: '5h ago',
-            commentCount: 5,
-            shareCount: 2,
-            },
-            {
-            id: 2,
-            title: "So you've bought coffee... now what?",
-            date: '2h ago',
-            commentCount: 3,
-            shareCount: 2,
-            },
-        ],
-        Users: [
-            {
-            id: 1,
-            title: 'Is tech making coffee better or worse?',
-            date: 'Jan 7',
-            commentCount: 29,
-            shareCount: 16,
-            },
-            {
-            id: 2,
-            title: 'The most innovative things happening in coffee',
-            date: 'Mar 19',
-            commentCount: 24,
-            shareCount: 12,
-            },
-        ],
-        Products: [
-            {
-            id: 1,
-            title: 'Ask Me Anything: 10 answers to your questions about coffee',
-            date: '2d ago',
-            commentCount: 9,
-            shareCount: 5,
-            },
-            {
-            id: 2,
-            title: "The worst advice we've ever heard about coffee",
-            date: '4d ago',
-            commentCount: 1,
-            shareCount: 2,
-            },
-        ],
-        Orders: [
-            {
-            id: 1,
-            title: 'Ask Me Anything: 10 answers to your questions about coffee',
-            date: '2d ago',
-            commentCount: 9,
-            shareCount: 5,
-            },
-            {
-            id: 2,
-            title: "The worst advice we've ever heard about coffee",
-            date: '4d ago',
-            commentCount: 1,
-            shareCount: 2,
-            },
-        ],
+        All: [],
+        Users: [],
+        Products: [],
+        Orders: [],
     })
 </script>
