528939c665509be80ee20f7e7b33ecfaf5785938
hangxdd <mrlivescreamlv@gmail.com>
2024-05-23 09:08:59 +0300

    Added icon for button

--- resources/js/views/AddOrderView.vue
+++ resources/js/views/AddOrderView.vue
@@ -77,8 +77,22 @@
               <button
                 @click.prevent="makeOrder"
                 type="submit"
-                class="shadow-md bg-emerald-600 hover:bg-emerald-500 p-3 px-6 rounded text-white text-center"
+                class="flex justify-center items-center shadow-md bg-emerald-600 hover:bg-emerald-500 p-3 px-6 rounded text-white text-center"
               >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-5 mr-1"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 4.5v15m7.5-7.5h-15"
+                  />
+                </svg>
                 Make order
               </button>
             </td>
